c7d3d06ad6bfabfcb5d323b0431f15dde45a474d
Eyob Shewangizaw <eyobsh72@gmail.com>
2023-05-14 04:38:38 -0500

    this is a commit right before migration

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import  JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# from .products import product
+#from .models import Product
+#from .serializers import *
 
-# Create your views here.
+# api_view(['POST'])
+# def createCustomer(request):
+#     user = 
+
