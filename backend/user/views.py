28adf92a01e30a57088072b986e34a5373fd8f46
Eyob Shewangizaw <eyobsh72@gmail.com>
2023-05-20 17:29:38 -0500

    migrate product model

--- backend/user/views.py
+++ backend/user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import  JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.contrib.auth.models import User, Group
 # from .products import product
 #from .models import Product
 #from .serializers import *
@@ -10,3 +11,11 @@ from rest_framework.response import Response
 # def createCustomer(request):
 #     user = 
 
+
+api_view(['POST'])
+def CustomerRegister(request):
+    data = request.data
+    registeringCustomer = User.objects.create(
+        user = data['body']
+            )
+    return Response("user created")
