ed22e16d633e6ee4978e83602f1f68d1fed37254
leburik <cherub777tarsa@yahoo.com>
2023-05-20 11:06:37 +0300

    create cart and cart_item class in cart.models

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# cart/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Cart(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    date_added = models.DateTimeField(auto_now_add=True)
