bfa2ab1effdd792d9a33d72dd653b24ffb636583
leburik <cherub777tarsa@yahoo.com>
2023-05-25 17:02:38 +0300

    import the product model to use it as foreign key

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-
-# cart/models.py
-from django.db import models
 from django.contrib.auth.models import User
+from product.models import Product
 
 
 class Cart(models.Model):
@@ -11,6 +9,6 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     date_added = models.DateTimeField(auto_now_add=True)
