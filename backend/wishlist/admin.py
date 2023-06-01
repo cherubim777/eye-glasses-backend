058a2557bf6e034009f7a6f45bfd606788205ddf
leburik <cherub777tarsa@yahoo.com>
2023-06-01 11:10:28 +0300

    make migrations and migrate the wishlist

--- backend/wishlist/admin.py
+++ backend/wishlist/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django .models import WishList, WishListItem
+from .models import WishList, WishListItem
 
 # Register your models here.
 admin.site.register(WishList)
