8169d8b114e5084632547e42223cc6454c3bf8bb
Eyob Shewangizaw <eyobsh72@gmail.com>
2023-06-17 17:14:35 -0500

    create static folder for images and set image url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -26,3 +28,5 @@ urlpatterns = [
     path("order/", include("order.urls")),
     path("wishlist/", include("wishlist.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
