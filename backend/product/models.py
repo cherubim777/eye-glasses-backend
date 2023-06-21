bac5d127a022082374b211fd7b231b58b7c74226
Eyob Shewangizaw <eyobsh72@gmail.com>
2023-06-21 02:23:38 -0500

    add default to rating

--- backend/product/models.py
+++ backend/product/models.py
@@ -62,7 +62,9 @@ class Product(models.Model):
     )
     brand = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True, default=0
+    )
     quantity = models.IntegerField(default=0)
     numReviews = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=7, decimal_places=2)
