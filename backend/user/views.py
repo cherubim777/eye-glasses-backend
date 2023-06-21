60fb172c87dc7dd894a9a87b77f738ca26b78f68
Eyob Shewangizaw <eyobsh72@gmail.com>
2023-06-20 23:27:53 -0500

    new fix for custom_order_price on register again

--- backend/user/views.py
+++ backend/user/views.py
@@ -149,59 +149,59 @@ def customerRegister(request):
 @api_view(["POST"])
 def retailerRegister(request):
     data = request.data
-    try:
-        with transaction.atomic():
-            user = User.objects.create(
-                username=data["username"],
-                password=make_password(data["password"]),
+    # try:
+    with transaction.atomic():
+        user = User.objects.create(
+            username=data["username"],
+            password=make_password(data["password"]),
+            email=data["email"],
+        )
+        if "photo" in data:
+            retailer = Retailer.objects.create(
+                user=user,
+                first_name=data["first_name"],
+                last_name=data["last_name"],
+                phone_number=data["phone_number"],
                 email=data["email"],
+                local_address=data["local_address"],
+                subcity=data["subcity"],
+                city=data["city"],
+                photo=data["photo"],
+                store_name=data["store_name"],
+                accepts_custom_order=data["accepts_custom_order"],
+                custom_order_price=None,
             )
-            if "photo" in data:
-                retailer = Retailer.objects.create(
-                    user=user,
-                    first_name=data["first_name"],
-                    last_name=data["last_name"],
-                    phone_number=data["phone_number"],
-                    email=data["email"],
-                    local_address=data["local_address"],
-                    subcity=data["subcity"],
-                    city=data["city"],
-                    photo=data["photo"],
-                    store_name=data["store_name"],
-                    accepts_custom_order=data["accepts_custom_order"],
-                    custom_order_price=None,
-                )
-            else:
-                retailer = Retailer.objects.create(
-                    user=user,
-                    first_name=data["first_name"],
-                    last_name=data["last_name"],
-                    phone_number=data["phone_number"],
-                    email=data["email"],
-                    local_address=data["local_address"],
-                    store_name=data["store_name"],
-                    subcity=data["subcity"],
-                    city=data["city"],
-                    accepts_custom_order=data["accepts_custom_order"],
-                    custom_order_price=None,
-                )
-            # Create an account for the new retailer with an initial balance of 0
-            # this is only to simulate maoney transaction
-            if "custom_order_price" in data:
-                retailer.custom_order_price = Decimal(data["custom_order_price"])
-            account = RetailerAccount.create(retailer=retailer, initial_balance=0)
-            user_serializer = UserSerializer(user, many=False)
-            retailer_serializer = RetailerSerializer(retailer, many=False)
-            account_serializer = RetailerAccountSerializer(account, many=False)
-            response_data = {
-                "user": user_serializer.data,
-                "retailer": retailer_serializer.data,
-                "account": account_serializer.data,
-            }
-            return Response(response_data)
-    except:
-        message = {"detail": "retailer with this username already exists"}
-        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            retailer = Retailer.objects.create(
+                user=user,
+                first_name=data["first_name"],
+                last_name=data["last_name"],
+                phone_number=data["phone_number"],
+                email=data["email"],
+                local_address=data["local_address"],
+                store_name=data["store_name"],
+                subcity=data["subcity"],
+                city=data["city"],
+                accepts_custom_order=data["accepts_custom_order"],
+                custom_order_price=None,
+            )
+        # Create an account for the new retailer with an initial balance of 0
+        # this is only to simulate maoney transaction
+        if "custom_order_price" in data:
+            retailer.custom_order_price = Decimal(data["custom_order_price"])
+        account = RetailerAccount.create(retailer=retailer, initial_balance=0)
+        user_serializer = UserSerializer(user, many=False)
+        retailer_serializer = RetailerSerializer(retailer, many=False)
+        account_serializer = RetailerAccountSerializer(account, many=False)
+        response_data = {
+            "user": user_serializer.data,
+            "retailer": retailer_serializer.data,
+            "account": account_serializer.data,
+        }
+        return Response(response_data)
+    # except:
+    #     message = {"detail": "retailer with this username already exists"}
+    #     return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["PUT"])
