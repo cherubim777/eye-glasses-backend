b4930066ffe0764ae5bf6f0934a15402a4398102
leburik <cherub777tarsa@yahoo.com>
2023-06-23 10:23:31 +0300

    change the default database to postgresq in settings and add the corresponding dependencey module name in requirement.txt file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,10 +128,25 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+'''
+this is for the postgresql databse config
+'''
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'visiondb',
+        'USER': 'kirubel',
+        'PASSWORD': '@leburik#',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
